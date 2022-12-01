c7754a94c94919addf4e445a662a81d737536441
gmcoates101 <gmcoates101@users.noreply.github.com>
2022-12-01 20:17:11 +0000

    Just messing

--- src/main/kotlin/worksheets/functions.ws.kts
+++ src/main/kotlin/worksheets/functions.ws.kts
@@ -4,19 +4,23 @@ import java.text.DateFormat
 import java.text.NumberFormat
 import java.util.*
 
-val tax = 1.2
-val products = mapOf(Pair("Water", 3.0), Pair("OJ", 7.89), Pair("Beer", 10.20), Pair("Whisky", 50.10), Pair("Wine", 35.12))
-val locale: Locale? = Locale.Builder().setLanguage("en").setRegion("GB").build()
-val currencyFormat: NumberFormat = NumberFormat.getCurrencyInstance(locale!!)
-val dateFormat: DateFormat = DateFormat.getDateInstance()
+val products = mapOf(
+        Pair("Water", 1.25),
+        Pair("Orange Juice", 3.10),
+        Pair("Beer", 3.80),
+        Pair("Whisky", 5.10),
+        Pair("Wine", 4.12))
 
 fun productPriceIncTax(name: String, price: Double) {
+    val tax = 1.2
+    val locale: Locale? = Locale.Builder().setLanguage("en").setRegion("GB").build()
+    val currencyFormat: NumberFormat = NumberFormat.getCurrencyInstance(locale!!)
+    val dateFormat: DateFormat = DateFormat.getDateInstance()
     val withTax = currencyFormat.format(price * tax)
+
     println("$name costs $withTax with tax at ${dateFormat.format(Date())}.\n")
 }
 
-fun productPriceIncTax(products: Map<String, Double>) {
-    products.forEach { productPriceIncTax(it.key, it.value) }
-}
+fun productPriceIncTax(products: Map<String, Double>) = products.forEach { productPriceIncTax(it.key, it.value) }
 
 productPriceIncTax(products)
